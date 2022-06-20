93f19c48dc3168b35f51e5c72c414c72fe6bd611
meekranjer <logicism81@gmail.com>
2022-06-20 22:47:03 +0800

    Added firebase AppCheck

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyClUNlJk64rKacwXvqxgFl5XcQfBkGmUjE",
-  authDomain: "color-palette-5c890.firebaseapp.com",
-  projectId: "color-palette-5c890",
-  storageBucket: "color-palette-5c890.appspot.com",
-  messagingSenderId: "245953098473",
-  appId: "1:245953098473:web:0b6a987402001397677678",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.RECAPTCHA),
+  isTokenAutoRefreshEnabled: true,
+});
+
 export default getFirestore();
