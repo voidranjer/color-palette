2930b6d0a5483cfcb43a0def92f6bd0b4dacf8d3
meekranjer <logicism81@gmail.com>
2022-06-24 03:28:26 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
